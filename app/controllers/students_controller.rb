c02f8081865a86a284b626465bb434c5c309353b
tuanlong1202 <tuanlong1202@gmail.com>
2021-08-28 23:50:47 -0400

    Phase 4 rails route params lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,22 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if !params[:name]
+      students = Student.all
+      render json: students
+    else
+      students = Student.all
+      render json: students.select{ |st| st.first_name == params[:name].capitalize || st.last_name == params[:name].capitalize } 
+    end
+  end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
+
+  def search
+    byebug
   end
 
 end
